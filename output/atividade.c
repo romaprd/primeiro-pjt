f9d1bc36936c3f6ae34e9f1bd89a6b632b95b0c4
romaprd <deyvyalex2006@gmail.com>
2024-09-05 17:24:23 -0300

    Add files via upload

---
+++ output/atividade.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+int c, cart, nMult, mCart, mnMult;
+float valor, total, totalGeral;
+
+mCart = 0;
+mnMult = 0;
+totalGeral = 0.0;
+ 
+ printf("digite a carteira de motorista ou 0 para terminar: ");
+ scanf("%d", cart);
+ 
+ if (cart < 4328){
+    while (cart != 0 && cart < 4328){
+total = 0.0;
+printf("digite o numero de mulltas: ");
+scanf("%d", &nMult);
+
+for (c = 1; c <= nMult; c++){
+    printf("digite o valor da %d", c, "")
+}
+
+    }
+   
+    
+ }
+
+
+
+
+
+
+    return 0;
+}
